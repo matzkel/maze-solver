f1c364366a20c86c467e43f7ba0871d1533cf975
matzkel <matzkel@proton.me>
2024-05-19 13:51:31 +0300

    Clean code and change scope of methods

--- src/graphics.py
+++ src/graphics.py
@@ -7,24 +7,34 @@ class Window():
 
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.protocol("WM_DELETE_WINDOW", self._close)
 
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        # Account for border width of lines
+        self._width = width + 2
+        self._height = height + 2
+
+        self.__canvas = Canvas(
+            self.__root,
+            bg="white",
+            width=self._width,
+            height=self._height
+        )
+
         self.__canvas.pack(fill=BOTH, expand=1)
 
     def draw_line(self, line, fill_color):
-        line.draw(self.__canvas, fill_color)
+        line._draw(self.__canvas, fill_color)
 
-    def redraw(self):
+    def _redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
         self.__running = True
         while self.__running:
-            self.redraw()
+            self._redraw()
 
-    def close(self):
+    def _close(self):
         self.__running = False
 
 
@@ -33,7 +43,7 @@ class Line():
         self.__point_a = point_a
         self.__point_b = point_b
 
-    def draw(self, canvas, fill_color):
+    def _draw(self, canvas, fill_color):
         canvas.create_line(
             self.__point_a.x,
             self.__point_a.y,
@@ -48,4 +58,3 @@ class Point():
     def __init__(self, x, y):
         self.x = x # x = 0 is the left of the screen
         self.y = y # y = 0 is the top of the screen
-
